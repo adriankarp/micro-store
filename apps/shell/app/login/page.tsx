85eaf6845e2a4c02075b3821d42cd3eefc539e6c
Adrian Karp <adrian.karp@ibm.com>
2025-04-16 12:08:47 +0300

    auth folder structure final

--- apps/shell/app/login/page.tsx
+++ apps/shell/app/login/page.tsx
@@ -1,7 +1,11 @@
-export default function Page() {
+import { LoginForm } from "@/components/login-form";
+
+export default function LoginPage() {
   return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">Login form here</div>
+    <div className="bg-muted flex min-h-svh flex-col items-center justify-center p-6 md:p-10">
+      <div className="w-full max-w-sm md:max-w-3xl">
+        <LoginForm />
+      </div>
     </div>
   );
 }
