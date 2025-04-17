904c10eb7963801bf515d530b0e5821110773835
Adrian Karp <adrian.karp@ibm.com>
2025-04-17 22:07:38 +0300

    typo

--- apps/shell/lib/validation.ts
+++ apps/shell/lib/validation.ts
@@ -20,7 +20,7 @@ export const loginFormSchema = yup
       .matches(/\d/, "Must contain at least one number")
       .matches(
         /[!@#$%^&*(),.?":{}|<>]/,
-        "Must contain at least one special character"
+        "Must contain at least one special character",
       )
       .matches(/^\S*$/, "Password cannot contain spaces"),
   })
@@ -49,7 +49,7 @@ export const registerFormSchema = yup
       .matches(/\d/, "Must contain at least one number")
       .matches(
         /[!@#$%^&*(),.?":{}|<>]/,
-        "Must contain at least one special character"
+        "Must contain at least one special character",
       )
       .matches(/^\S*$/, "Password cannot contain spaces"),
 
