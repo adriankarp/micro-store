15ba5e652a4300873178d8eccbb9df2325c500e1
Adrian Karp <adrian.karp@ibm.com>
2025-04-18 18:18:42 +0300

    logout functionality

--- apps/storefront/app/page.tsx
+++ apps/storefront/app/page.tsx
@@ -1,13 +1,23 @@
+"use client";
+
 import { Button } from "@micro-store/ui/components/button";
+import { clearTokens } from "@micro-store/utils/cookies";
+import { useRouter } from "next/navigation";
 
 export default function Page() {
+  const router = useRouter();
+
+  const logout = () => {
+    clearTokens();
+    router.push("/login");
+  };
   return (
     <div className="flex items-center justify-center min-h-svh">
       <div className="flex flex-col items-center justify-center gap-4">
         <h1 className="text-2xl font-bold">Storefront</h1>
-        <a href="/login">
-          <Button size="sm">Go to login</Button>
-        </a>
+        <Button size="sm" onClick={logout}>
+          Logout
+        </Button>
       </div>
     </div>
   );
